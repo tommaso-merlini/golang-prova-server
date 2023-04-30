41a3ecffa75e025d30472c46f2faa7ce5683ae4e
tommaso-merlini <tommaso.merlini.2004@gmail.com>
2023-04-30 20:36:54 +0200

    fixed mongodb init

--- main.go
+++ main.go
@@ -3,40 +3,42 @@ package main
 import (
 	"fmt"
 	"log"
+	"server/db"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/kamva/mgm/v3"
 )
 
 type Product struct {
-    mgm.DefaultModel `bson:",inline"`
-	Name string `json:"name" bson:"name"`
+	mgm.DefaultModel `bson:",inline"`
+	Name             string `json:"name" bson:"name"`
 }
 
 func main() {
-   app := fiber.New()
+	app := fiber.New()
+	db.Init()
 
-   app.Get("/", func(c *fiber.Ctx) error {
-        return c.JSON(fiber.Map{
-            "status": "ok",
-        })
-    });
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.JSON(fiber.Map{
+			"status": "ok",
+		})
+	})
 
-    app.Get("/product/:id", func(c *fiber.Ctx) error {
-        product := &Product{}
-        coll := mgm.Coll(product)
+	app.Get("/product/:id", func(c *fiber.Ctx) error {
+		product := &Product{}
+		coll := mgm.Coll(product)
 
-        // Find and decode the doc to a book model.
-        err := coll.FindByID(c.Params("id"), product)
+		// Find and decode the doc to a book model.
+		err := coll.FindByID(c.Params("id"), product)
 
-        if err != nil {
-            fmt.Println(err)
-        }
-        
-        return c.JSON(product)
-    });
+		if err != nil {
+			fmt.Println(err)
+		}
 
-    app.Static("/loaderio-04cbc2e6e8994582817d57faa8742ee5", "/loaderio-04cbc2e6e8994582817d57faa8742ee5.html")
+		return c.JSON(product)
+	})
 
-    log.Fatal(app.Listen(":3000"))
+	app.Static("/loaderio-04cbc2e6e8994582817d57faa8742ee5", "/loaderio-04cbc2e6e8994582817d57faa8742ee5.html")
+
+	log.Fatal(app.Listen(":3000"))
 }
