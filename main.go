044d95a010e35828ad0415ebe56b5d3dc88e6d69
tommaso-merlini <tommaso.merlini.2004@gmail.com>
2023-05-04 22:34:37 +0200

    .

--- main.go
+++ main.go
@@ -1,13 +1,7 @@
 package main
 
 import (
-<<<<<<< HEAD
 	"net/http"
-=======
-	"fmt"
-	"log"
-	"server/db"
->>>>>>> 41a3ecffa75e025d30472c46f2faa7ce5683ae4e
 
 	"github.com/gin-gonic/gin"
 	"github.com/kamva/mgm/v3"
@@ -19,7 +13,6 @@ type Product struct {
 }
 
 func main() {
-<<<<<<< HEAD
 
     // app.Get("/product/:id", func(c *fiber.Ctx) error {
     //     product := &Product{}
@@ -46,32 +39,3 @@ func main() {
 
     r.Run(":3000") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
-=======
-	app := fiber.New()
-	db.Init()
-
-	app.Get("/", func(c *fiber.Ctx) error {
-		return c.JSON(fiber.Map{
-			"status": "ok",
-		})
-	})
-
-	app.Get("/product/:id", func(c *fiber.Ctx) error {
-		product := &Product{}
-		coll := mgm.Coll(product)
-
-		// Find and decode the doc to a book model.
-		err := coll.FindByID(c.Params("id"), product)
-
-		if err != nil {
-			fmt.Println(err)
-		}
-
-		return c.JSON(product)
-	})
-
-	app.Static("/loaderio-04cbc2e6e8994582817d57faa8742ee5", "/loaderio-04cbc2e6e8994582817d57faa8742ee5.html")
-
-	log.Fatal(app.Listen(":3000"))
-}
->>>>>>> 41a3ecffa75e025d30472c46f2faa7ce5683ae4e
